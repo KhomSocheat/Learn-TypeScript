34cd041d2cf7b9409eeaa128a9dd23a8a23292a4
Khom Socheat <khomsocheat@acledaaib.onmicrosoft.com>
2026-02-07 21:36:01 +0700

    Optional param in function

--- src/function.ts
+++ src/function.ts
@@ -2,4 +2,12 @@ function multiply(a: number, b: number){
     return a * b;
 }
 
+function multiply2(a:number, b?:number):number{
+    if(b){ //if b doesn't have value so it's only return a
+        return a * b;
+    }else{
+        return a;
+    }
+}
+
 console.log(multiply(10,20));
