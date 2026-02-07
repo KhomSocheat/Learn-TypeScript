66edcbdb1673521beff6ac8e3be02cd3a51e34a5
Khom Socheat <khomsocheat@acledaaib.onmicrosoft.com>
2026-02-07 21:29:59 +0700

    normal function with type

--- dist/basic_type.js
+++ dist/basic_type.js
@@ -1,5 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+// declare "name"
+let y;
+// dclare "name" : "type"
+let b;
+// declare "name" = value
+let z = true;
+// declare "name" : type = value
+let a = "";
 let x = 12;
 console.log(typeof x);
+function printHello() {
+    console.log("hello world");
+}
 //# sourceMappingURL=basic_type.js.map
--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 console.log("Hello World");
-// declare "name"
-let y;
-// dclare "name" : "type"
-let x;
-// declare "name" = value
-let z = true;
-// declare "name" : type = value
-let a = "";
 //# sourceMappingURL=index.js.map
